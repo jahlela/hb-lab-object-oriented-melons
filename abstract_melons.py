b0acb11a438cde98cfa756e3c7f4952527b54993
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-06 17:28:13 -0700

    Updated get_total to reflect price increases for Christmas melons and international orders with fewer than 10 melons.

--- abstract_melons.py
+++ abstract_melons.py
@@ -7,19 +7,26 @@ class AbstractMelonOrder(object):
 
 
 
-    def __init__(self, species, qty, country_code = None):
+    def __init__(self, species, qty, country_code = 'USA'):
         """Initialize melon order attributes"""
 
         self.species = species
         self.qty = qty
         self.shipped = False
         # Should this go here? 
-        self.country_code = None
+        self.country_code = country_code
 
     def get_total(self):
         """Calculate price."""
 
         base_price = 5
+
+        if self.species == "christmas": 
+            base_price = base_price * 1.5
+
+        if self.order_type == "international" and self.qty < 10:
+            base_price = base_price + 3
+
         total = (1 + self.tax) * self.qty * base_price
         return total
 
@@ -43,13 +50,6 @@ class InternationalMelonOrder(AbstractMelonOrder):
 
     order_type = "international"
     tax = 0.17
-    # country_code = country_code
-
-
-    def __init__(self, species, qty, country_code):
-        """Initialize melon order attributes"""
-        self.country_code = country_code
-
 
     def get_country_code(self):
         """Return the country code."""
